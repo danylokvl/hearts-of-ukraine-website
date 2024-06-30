22157b6fbadc6d3c92ac1dded02f033915eea080
danylokvl <danylokvl@gmail.com>
2024-06-30 14:02:41 +0300

    MainActivitiesBlock is started

---
+++ src/components/MainPage-Sections/MainActivitiesBlock/MainActivitiesBlock.jsx
@@ -0,0 +1,53 @@
+import "./MainActivitiesBlock.scss";
+
+const MainActivitiesBlock = () => {
+  return (
+    <section className="mainActivitiesBlock">
+      <div className="mainActivitiesBlock__content">
+        <div className="mainActivitiesBlock__cardsContainer">
+          <div className="mainActivitiesBlock__card">
+            <div className="textContaienr">
+              <h3>Підтримайте нас фінансово</h3>
+              <p>
+                Кожен внесок, великий чи малий, має значення і робить реальний вплив на життя тих,
+                хто цього потребує.
+              </p>
+            </div>
+            <button>
+              <span>Внести Кошти</span>
+              <img />
+            </button>
+          </div>
+          <div className="mainActivitiesBlock__card">
+            <div className="textContaienr">
+              <h3>Підтримайте нас фінансово</h3>
+              <p>
+                Кожен внесок, великий чи малий, має значення і робить реальний вплив на життя тих,
+                хто цього потребує.
+              </p>
+            </div>
+            <button>
+              <span>Внести Кошти</span>
+              <img />
+            </button>
+          </div>
+          <div className="mainActivitiesBlock__card">
+            <div className="textContaienr">
+              <h3>Підтримайте нас фінансово</h3>
+              <p>
+                Кожен внесок, великий чи малий, має значення і робить реальний вплив на життя тих,
+                хто цього потребує.
+              </p>
+            </div>
+            <button>
+              <span>Внести Кошти</span>
+              <img />
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MainActivitiesBlock;
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,10 +1,12 @@
 import HeroSection from "../../components/MainPage-Sections/HeroSection/HeroSection";
+import MainActivitiesBlock from "../../components/MainPage-Sections/MainActivitiesBlock/MainActivitiesBlock";
 import "./MainPage.scss";
 
 const MainPage = () => {
   return (
     <section className="mainPage">
       <HeroSection />
+      <MainActivitiesBlock />
     </section>
   );
 };
