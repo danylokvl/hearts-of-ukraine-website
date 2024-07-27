09d300553e54b54d4dbb6004ca113c230ff34333
danylokvl <danylokvl@gmail.com>
2024-07-27 17:09:38 +0300

    volunteer data sends to email, but it needs to work with errors

---
+++ src/DATA.js
@@ -0,0 +1,5 @@
+export const DATA = {
+  service_id: "service_qftmpfh",
+  template_id: "template_bzxbgwp",
+  user_id: "i1UslDJTGN6T73HNh",
+};
--- src/components/MainPage-Popups/BecomeVolunteerPopup/BecomeVolunteerPopup.js
+++ src/components/MainPage-Popups/BecomeVolunteerPopup/BecomeVolunteerPopup.js
@@ -1,9 +1,37 @@
-import PopupCloseButton from "../../ui/PopupCloseButton/PopupCloseButton";
-import "./BecomeVolunteerPopup.scss";
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
 import TextField from "@mui/material/TextField";
 import PrimaryButton from "../../ui/PrimaryButton/PrimaryButton";
+import PopupCloseButton from "../../ui/PopupCloseButton/PopupCloseButton";
+import "./BecomeVolunteerPopup.scss";
+import { DATA } from "../../../DATA";
 
 const BecomeVolunteerPopup = () => {
+  const [volunteerData, setVolunteerData] = useState({
+    name: "",
+    phoneNumber: "",
+    email: "",
+    city: "",
+    summary: "",
+  });
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const response = await emailjs
+      .send(DATA.service_id, DATA.template_id, volunteerData, DATA.user_id)
+      .finally(() =>
+        setVolunteerData({
+          name: "",
+          phoneNumber: "",
+          email: "",
+          city: "",
+          summary: "",
+        })
+      );
+
+    console.log(response);
+  }
+
   return (
     <section className="mainPagePopup becomeVolunteerPopup">
       <div className="becomeVolunteerPopup__content">
@@ -18,25 +46,58 @@ const BecomeVolunteerPopup = () => {
             label="Ваші ім'я та прізвище"
             variant="standard"
             required
+            autoComplete="off"
+            value={volunteerData.name}
+            onChange={(e) =>
+              setVolunteerData((prevVolunteerData) => ({
+                ...prevVolunteerData,
+                name: e.target.value,
+              }))
+            }
           />
           <TextField
             id="phoneNumberTextField"
             className="mainPagePopup__textField"
             label="Ваш номер телефону"
             variant="standard"
+            autoComplete="off"
             required
+            value={volunteerData.phoneNumber}
+            onChange={(e) =>
+              setVolunteerData((prevVolunteerData) => ({
+                ...prevVolunteerData,
+                phoneNumber: e.target.value,
+              }))
+            }
           />
           <TextField
-            id="countryTextField"
+            id="emailTextField"
             className="mainPagePopup__textField"
-            label="Країна, в якій ви знаходитесь"
+            label="Ваш Email"
             variant="standard"
+            autoComplete="off"
+            required
+            value={volunteerData.email}
+            onChange={(e) =>
+              setVolunteerData((prevVolunteerData) => ({
+                ...prevVolunteerData,
+                email: e.target.value,
+              }))
+            }
           />
           <TextField
             id="cityTextField"
             className="mainPagePopup__textField"
-            label="Місто, де ви знаходитесь"
+            label=" Назва населеного пункту, де ви знаходитесь"
+            autoComplete="off"
             variant="standard"
+            value={volunteerData.city}
+            onChange={(e) =>
+              setVolunteerData((prevVolunteerData) => ({
+                ...prevVolunteerData,
+                city: e.target.value,
+              }))
+            }
           />
           <TextField
             id="summaryTextField"
@@ -44,10 +105,24 @@ const BecomeVolunteerPopup = () => {
             label="Розкажіть про себе"
             multiline
             variant="standard"
+            autoComplete="off"
+            value={volunteerData.summary}
+            onChange={(e) =>
+              setVolunteerData((prevVolunteerData) => ({
+                ...prevVolunteerData,
+                summary: e.target.value,
+              }))
+            }
           />
-          <PrimaryButton type="submit" id="becomeVolunteerBtn">
-            СТАТИ ВОЛОНТЕРОМ
-          </PrimaryButton>
+          <div className="becomeVolunteerPopup__btnContainer">
+            <p>
+              Натискаючи на кнопку “СТАТИ ВОЛОНТЕРОМ” ви надаєте згоду на обробку ваших персональних
+              даних
+            </p>
+            <PrimaryButton type="submit" id="becomeVolunteerBtn" onClick={handleSubmit}>
+              СТАТИ ВОЛОНТЕРОМ
+            </PrimaryButton>
+          </div>
         </form>
       </div>
     </section>
